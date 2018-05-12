264678251d2e2aeb7bf29cd75b68b46637e815a5
Pineapples <37296491+Howtoeatpineapples@users.noreply.github.com>
2018-05-12 14:12:28 +0800

    Add files via upload

---
+++ Crypt.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace XORCrypt
+{
+    class Crypt
+    {
+       
+        public byte[] PrivateKey { get; private set; }
+        
+
+        public void InputPassphrase(string passPhrase)
+        {
+            HashAlgorithm sha256 = new SHA256CryptoServiceProvider();
+            byte[] buffer = sha256.ComputeHash(Encoding.UTF8.GetBytes(passPhrase));
+            PrivateKey = buffer;
+        }
+
+
+        public void LoadFile(string filePath)
+        {
+            GenerateKey(File.ReadAllBytes(filePath).Length);
+        }
+
+        public void EncryptDecryptFile(string filePath, string fileName)
+        {
+            File.Create(filePath + "output.txt").Close();
+
+            File.WriteAllBytes(filePath + "output.txt", EncryptDecrypt(File.ReadAllBytes(filePath + fileName)));
+            
+            File.Delete(filePath + fileName);
+            File.Move(filePath + "output.txt", filePath + fileName);
+        }
+         
+        public byte[] EncryptDecrypt(byte[] input)
+        {
+            byte[] output = new byte[input.Length];
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                output[i] = (byte)(input[i] ^ PrivateKey[i % PrivateKey.Length]);
+            }
+
+            return output;
+        }
+
+
+        private void GenerateKey(int size)
+        {
+            RNGCryptoServiceProvider rNGCryptoServiceProvider = new RNGCryptoServiceProvider();
+            byte[] keyBuffer = new byte[size];
+            rNGCryptoServiceProvider.GetBytes(keyBuffer);
+            PrivateKey = keyBuffer;
+        }
+
+        public static string FileToString(string filePath)
+        {
+            var reader = new System.IO.StreamReader(filePath, System.Text.Encoding.UTF8);
+            var text = reader.ReadToEnd();
+
+            reader.Close();
+            return text;
+        }
+
+        public static void StringToFile(string filePath, string text)
+        {
+            var writer = new System.IO.StreamWriter(filePath, false, System.Text.Encoding.UTF8);
+
+            writer.Write(text);
+            writer.Close();
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace XORCrypt
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            
+
+            Crypt crypt = new Crypt();
+            
+            Console.Write("Please enter your passphrase: ");
+            crypt.InputPassphrase(Console.ReadLine());
+
+            Console.Clear();
+
+            string dir = string.Empty;
+            string fileName = string.Empty;
+            do {
+                Console.Clear();
+                Console.Write("Please write the full directory the file is in: ");
+                dir = Console.ReadLine();
+                Console.Write("Please write the name of the file you wish to encrypt: ");
+                fileName = Console.ReadLine();
+            } while (!File.Exists(dir + fileName));
+
+            while (true)
+            {
+                Console.WriteLine("Press any key to encrypt/decrypt");
+                Console.ReadKey();
+                crypt.EncryptDecryptFile(dir, fileName);
+                Console.Clear();
+                Console.WriteLine("File encrypted/decrypted");
+            }
+        }
+    }
+}
